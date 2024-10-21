430e4bd22c1234fcdd39c39ac4a5ef0ea391c06c
criscuarez01 <calcuarezma@uamv.edu.ni>
2024-10-21 08:46:26 -0600

    Tarea del 21

---
+++ 3 ejercicios guia didactica semana 9/Ejercicio 2 guia semana 9/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+
+class CuentaBancaria
+{
+    class Transaccion
+    {
+        public string Tipo { get; set; }
+        public decimal Monto { get; set; }
+
+        public Transaccion(string tipo, decimal monto)
+        {
+            Tipo = tipo;
+            Monto = monto;
+        }
+
+        public override string ToString()
+        {
+            return $"{Tipo}: {Monto:C}";
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        decimal saldo = 0;
+        int opcion = 0;
+
+        List<Transaccion> transacciones = new List<Transaccion>();
+
+        do
+        {
+            Console.WriteLine("\nMenú de opciones:");
+            Console.WriteLine("1. Consultar saldo");
+            Console.WriteLine("2. Depositar dinero");
+            Console.WriteLine("3. Retirar dinero");
+            Console.WriteLine("4. Ver historial de transacciones");
+            Console.WriteLine("5. Salir");
+            Console.Write("Seleccione una opción: ");
+
+       
+            opcion = Convert.ToInt32(Console.ReadLine());
+
+            switch (opcion)
+            {
+                case 1:
+                    Console.WriteLine($"Su saldo actual es: {saldo:C}");
+                    break;
+
+                case 2:
+                    Console.Write("Ingrese la cantidad a depositar: ");
+                    decimal deposito = Convert.ToDecimal(Console.ReadLine());
+                    saldo += deposito;
+                    transacciones.Add(new Transaccion("Depósito", deposito)); 
+                    Console.WriteLine($"Depósito realizado. Su nuevo saldo es: {saldo:C}");
+                    break;
+
+                case 3:
+                    Console.Write("Ingrese la cantidad a retirar: ");
+                    decimal retiro = Convert.ToDecimal(Console.ReadLine());
+
+                   
+                    if (retiro > saldo)
+                    {
+                        Console.WriteLine("Fondos insuficientes.");
+                    }
+                    else
+                    {
+                        saldo -= retiro;
+                        transacciones.Add(new Transaccion("Retiro", retiro)); 
+                        Console.WriteLine($"Retiro realizado. Su nuevo saldo es: {saldo:C}");
+                    }
+                    break;
+
+                case 4:
+                    Console.WriteLine("Historial de transacciones:");
+                    if (transacciones.Count == 0)
+                    {
+                        Console.WriteLine("No se han realizado transacciones.");
+                    }
+                    else
+                    {
+                        foreach (var transaccion in transacciones)
+                        {
+                            Console.WriteLine(transaccion);
+                        }
+                    }
+                    break;
+
+                case 5:
+                    Console.WriteLine("Acciones ejecutadas correctamente. ¡Hasta pronto!");
+                    break;
+
+                default:
+                    Console.WriteLine("Opción no válida, por favor intente nuevamente.");
+                    break;
+            }
+        } while (opcion != 5);
+    }
+}
---
+++ 3 ejercicios guia didactica semana 9/Semana 9- Lista diccionario/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioDiccionarioT
+{
+    internal class DiccionarioT
+    {
+        static void Main()
+        {
+            List<Tuple<string, string>> diccionario = CrearDiccionario();
+            traducir(diccionario);
+        }
+
+        public static List<Tuple<string, string>> CrearDiccionario()
+        {
+            List<Tuple<string, string>> diccionario = new List<Tuple<string, string>>();
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Introduzca la palabra {i + 1} en ingles: ");
+                string palabra1 = Console.ReadLine();
+
+                Console.WriteLine($"Introduzca la palabra {i + 1} en español: ");
+                string palabra2 = Console.ReadLine();
+
+                diccionario.Add(new Tuple<string, string>(palabra1, palabra2));
+            }
+            return diccionario;
+        }
+
+        public static void traducir(List<Tuple<string, string>> diccionario)
+        {
+            Console.WriteLine("Introduzca la palabra a traducir: ");
+            string palcomp = Console.ReadLine();
+            bool encontrado = false;
+
+            foreach (var duo in diccionario)
+            {
+                if (duo.Item1.Equals(palcomp, StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine($"La traduccion de la palabra {palcomp} es: {duo.Item2}.");
+                    encontrado = true;
+                    break;
+                }
+                else if (duo.Item2.Equals(palcomp, StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine($"La traduccion de la palabra {palcomp} es: {duo.Item1}.");
+                    encontrado = true;
+                    break;
+                }
+            }
+
+            if (!encontrado)
+            {
+                Console.WriteLine($"La palabra {palcomp} no se encuentra en el diccionario.");
+            }
+        }
+    }
+}
